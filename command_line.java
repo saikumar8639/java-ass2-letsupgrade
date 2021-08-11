0f323eb5afdbb7e6b91801a72c914ccec76dfa87
saikumar8639 <75746273+saikumar8639@users.noreply.github.com>
2021-08-11 15:51:05 +0530

    Add files via upload

---
+++ command_line.java
@@ -0,0 +1,9 @@
+class command_line
+{
+public static void main(String args[])
+{
+int a=Integer.parseInt(args[0]);
+int b=Integer.parseInt(args[1]);
+System.out.println("The sum of two numbers is "+(a+b));
+}
+}
